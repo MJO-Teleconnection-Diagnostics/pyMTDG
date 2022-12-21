50f7c8ac6da0285b78eba72873b53ffb85a8c3d0
Cristiana Stan <31143522+cristianastan2@users.noreply.github.com>
2022-12-21 15:16:34 -0500

    Create fcst_utils.py

---
+++ Code/fcst_utils.py
@@ -0,0 +1,44 @@
+###Usage: In the main code add <from fcst_utils import *>
+
+import numpy as np
+import xarray as xr
+
+def calcAnom(ds,var_name,anom_name):
+    # Save the original dates 
+    time=ds.time
+    
+    # Get the time dimensions; ntimes=# of samples; nyrs=# of calendar years; nlead=# of foreecast leads
+    ntimes=len(time)
+    index=ds.time['time.year']
+    yStrt=index[0]
+    yLast=index[-1]
+    nyrs=int(yLast-yStrt)
+    nlead=int(len(index)/nyrs)
+    
+    # Compute climatology for each forecast lead 
+    climo=[]
+    for i in range(nlead):
+        da=ds[var_name][i:ntimes-1:nlead,:,:].mean(dim='time')
+        climo.append(da.to_dataset(name='clim'))
+    ds_clim=xr.combine_nested(climo,concat_dim='time')
+    
+    # Change time coordinate from calendar dates to integers [0:ntimes]
+    days=[]
+    for i in range(nyrs):
+        days.append(np.arange(nlead))
+    days=np.reshape(days,nyrs*nlead)
+    
+    ds['time']=days
+    
+    # Compute anomalies 
+    anoms=ds[var_name].groupby(ds.time)-ds_clim
+    
+    # Rename anomaly 
+    anom=anoms['clim'].rename(anom_name)
+    
+    # Remap time coordinate to original calendar dates
+    anom['time']=time
+    
+    return anom
+
+  ### Plan to add the regridding coded shareed by Cheng
