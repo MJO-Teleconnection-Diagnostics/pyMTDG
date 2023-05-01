78d043cca148ce5c4a34760eb4257d8dfef939c8
Andrea Jenney <ajenney@uci.edu>
2023-05-01 17:42:58 -0400

    added code for STRIPES consistent with flow chart

---
+++ STRIPES/STRIPES_utils.py
@@ -0,0 +1,240 @@
+# Compute the STRIPES index for a gridded variable given
+# comp_anom -  an array of composite anomalies. Must be ordered
+#                (phases, lags, nlat, nlon)
+# pspeed    -  a vector of speeds (how many days does the mjo spend in phase)
+
+import numpy as np
+import xarray as xr
+import pandas as pd
+from datetime import timedelta
+
+# ===============================================================================================
+def calc_lagged_composite(data, amp, phase, maxlag, minlag=0, nphases=8, obs=False):
+    '''
+    Computes the lagged composite matrix 
+    
+        Parameters:
+            data: xarray data with named dimensions 'time' (initialization day)
+                  and 'forecast_day' (lead time) if forecast data
+            amp: boolean xarray vector of length n of MJO amplitudes exceeding threshold
+                 !!note: time dimension must be named!!
+            phase: vector of length n of MJO phases
+            maxlag: maximum lag to composite on 
+            minlag: minimum lag to composite on, default=0
+            nphases: number of mjo phases, default=8
+            obs: boolean indicating whether obs or forecast (default)
+            
+        Returns:
+            lagcomp: lagged composite matrix
+    '''
+    
+    if obs:
+        lag = np.arange(minlag, maxlag+1)
+    
+    lagcomp = []
+    for iphase in range(nphases):
+        inds = np.squeeze(np.argwhere((amp.values) & (phase==(iphase+1))))
+        
+        if obs:
+            t0=pd.to_datetime(amp.time[inds])+timedelta(days=minlag)
+            t1=pd.to_datetime(amp.time[inds])+timedelta(days=maxlag)
+            
+            comp = []
+            for ievent in range(len(t0)):
+                subset = data.sel(time=slice(t0[ievent], t1[ievent])).rename(time='lag')
+                subset['lag']=lag
+                comp.append(subset)
+            lagcomp.append(xr.concat(comp, dim='event').mean('event'))
+        else:
+            lagcomp.append(data.isel(time=inds, 
+                                     forecast_day=slice(minlag, 
+                                                        maxlag+1)).mean('time'))
+    lagcomp=xr.concat(lagcomp,dim='phase')
+    lagcomp['phase']=np.arange(1,nphases+1)
+
+    return lagcomp
+    
+# ===============================================================================================
+def compSTRIPES1D(comp_anom,pspeed):
+    '''
+    Compute the STRIPES index for a variable with inputs:
+        comp_anom -  an array of composite anomalies. Must be ordered
+                     (phases, lags). Must be a numpy matrix
+        pspeed    -  a vector of speeds (how many days does the mjo spend in phase)    
+    '''
+
+    # Make sure pspeed is an array
+    try: len(pspeed)
+    except: pspeed=[pspeed]
+    
+    nphases,nlags = comp_anom.shape
+
+    # calculate the maximum number of samples we can get in 
+    # diagonal means. New in this computation of STRIPES 
+    # (not in Jenney et al. (2019))
+    nsample = int(nlags/nphases)
+
+    # instead of reordering as in Jenney et al. (2019), stack the lagged
+    # composite matrix on the phase axis nphases number of times. This
+    # gives a 30x speedup
+    comp_anom_stack=np.tile(comp_anom,(nphases,1))
+    nphases = comp_anom_stack.shape[0] # for creating the shifted array
+
+    shiftsum_var = []
+    # Loop through each speed and compute the diagonal mean
+    for ispeed, speed in enumerate(pspeed):
+        n_diag=nlags + (nphases-1)*speed
+        shiftsum=[]
+        for idiag in range(n_diag):
+            # First diagonals are the first SPEED lags of the first phase
+            if idiag < n_diag-nlags:
+                iphases = np.arange(np.floor(idiag/speed),-1,-1,dtype='uint8')
+                ilags = np.arange(idiag%speed,nlags,speed,dtype='uint8')
+            # Last diagonals are the entire row in the last phase
+            else:
+                iphases = np.arange(7,-1,-1,dtype='uint8')
+                ilags = np.arange(idiag-((nphases-1)*speed),nlags,speed,
+                                  dtype='uint8')
+
+            n_items = min(len(iphases), len(ilags))
+            shiftsum.append(np.mean(comp_anom_stack[iphases[:n_items], 
+                                                    ilags[:n_items]]))
+
+        # Compute the variance
+        shiftsum_var.append(np.var(np.asarray(shiftsum)))
+
+    stripes = np.sqrt(2*np.nanmean(np.asarray(shiftsum_var)))
+    return(stripes)
+    
+# ===============================================================================================
+def compSTRIPES2D(comp_anom,pspeed):
+    '''
+    Compute the STRIPES index for a 2D variable
+       comp_anom -  an array of composite anomalies. Must be ordered
+                    (phases, lags, lat/lon, lon/lat)
+       pspeed    -  a vector of speeds (how many days does the mjo spend in phase)    
+    '''
+
+    # Make sure pspeed is a vector
+    try: len(pspeed)
+    except: pspeed=[pspeed]
+
+    nphases,nlags = comp_anom.shape[:2]
+    
+    # instead of reordering as in Jenney et al. (2019), stack the lagged
+    # composite matrix on the phase axis nphases number of times. This
+    # gives a 30x speedup
+    comp_anom_stack=np.tile(comp_anom,(nphases,1,1,1))
+    nphases = comp_anom_stack.shape[0] # for creating the shifted array
+    
+    diagonal_mean_var = []
+    # Loop through each speed and compute the diagonal mean
+    for speed in pspeed:
+        n_diag=nlags + (nphases-1)*speed
+        diagonal_mean=[]
+        n_in_diagonal=[]
+        
+        # Loop through each diagonal
+        for idiag in range(n_diag):
+            # Find the indices of the diagonal
+            # First diagonals are the first SPEED lags of the first phase
+            if idiag < n_diag-nlags:
+                iphases = np.arange(np.floor(idiag/speed),-1,-1,dtype='uint8')
+                ilags = np.arange(idiag%speed,nlags,speed,dtype='uint8')
+            # Last diagonals are the entire row in the last phase
+            else:
+                iphases = np.arange(7,-1,-1,dtype='uint8')
+                ilags = np.arange(idiag-((nphases-1)*speed),nlags,speed,
+                                  dtype='uint8')
+                
+            n_items = min(len(iphases), len(ilags))
+            diagonal_mean.append(np.mean(comp_anom_stack[iphases[:n_items], 
+                                        ilags[:n_items],:,:],
+                        axis=0))
+            
+        # After calculating all of the diagonal means, compute the 
+        # variance of the resulting vector for the current speed
+        diagonal_mean_var.append(np.var(np.asarray(diagonal_mean),axis=0))
+        
+    # After computing the variance for each speed, compute the mean 
+    # variance, and then convert the variance to the STRIPES amplitude
+    stripes = np.sqrt(2*np.nanmean(np.asarray(diagonal_mean_var),axis=0))
+
+    return(stripes)
+# ===============================================================================================
+def compSTRIPES(comp_anom,pspeed):
+    '''
+    Computes the STRIPES index
+    
+        Parameters:
+            comp_anom: matrix of lagged composite with first 2 dims (phases, lags)
+                       input can be 2D or 4D
+            pspeed: vector or scalar of speeds (# of days does the mjo spends in a phase)
+            
+        Returns:
+            stripes: stripes index
+    '''
+
+    if len(np.shape(comp_anom)) == 2:
+        stripes = compSTRIPES1D(comp_anom,pspeed)
+    elif len(np.shape(comp_anom)) == 4:
+        stripes = compSTRIPES2D(comp_anom,pspeed)
+                                                 
+    return(stripes)
+# ===============================================================================================
+def bootstrapSTRIPES(anoms, nboot, rmm, minlag, maxlag):
+    '''
+    Block bootstrap (using random shuffles of the years) computation
+    of the STRIPES index
+    
+        Parameters:
+            anoms: data array of forecast anomalies, must have dimension named
+                   "time"
+            nboot: number of bootstrap samples, each one takes about 10 seconds
+            rmm: data array of rmm phases and amplitudes 
+            minlag: minimum lag used for composites
+            maxlag: maximum lag used for composites
+            
+        Returns:
+            stripes_boot: nboot * nlat * nlon array of STRIPES indices
+    '''
+    print('computing bootstrap STRIPES significance...\n estimated time to complete = ' 
+          + str(10*nboot/60) + ' min.')
+    
+    # Reorder forecast data by year
+    groups = anoms.groupby('time.year')
+    data_by_year = [group[1] for group in groups]
+    nyr = len(data_by_year)
+    ntime = len(anoms.time)
+    
+    stripes_boot = []
+    for iboot in range(nboot):
+        inds_shuffle = np.random.choice(nyr,nyr,replace=True)
+        anoms_shuffle_year = xr.concat([data_by_year[idx] for idx in inds_shuffle], dim='time')
+        
+        # make sure the time dimension is the correct size, if it isn't ei ther shorten it
+        # or try again
+        ntime_boot = len(anoms_shuffle_year.time)
+        while ntime_boot != ntime:
+            if ntime_boot > ntime:
+                anoms_shuffle_year = anoms_shuffle_year.isel(time=slice(0,ntime))
+            else:
+                inds_shuffle = np.random.choice(nyr,nyr,replace=True)
+                anoms_shuffle_year = xr.concat([data_by_year[idx] 
+                                                for idx in inds_shuffle], dim='time')
+            ntime_boot = len(anoms_shuffle_year.time)
+            
+        # Calculate lagged composite with the bootstrap sample
+        lagcomp = calc_lagged_composite(anoms_shuffle_year,
+                                        rmm.amplitude>1.0,
+                                        rmm.phase.values,
+                                        maxlag,
+                                        minlag)
+        # Calculate STRIPES index
+        stripes_boot.append(compSTRIPES(lagcomp.values, [5,6,7,8]))
+        
+    # Concatenate bootstrap samples
+    stripes_boot = np.stack(stripes_boot)
+    
+    return stripes_boot
+    
---
+++ STRIPES/fcst_utils.py
@@ -0,0 +1,209 @@
+##Usage: In the main code add from fcst_utils import *
+
+import numpy as np
+import xarray as xr
+import spharm
+
+def calcAnom(ds,anom_name):
+    # Save the original dates 
+    time=ds.time
+    
+    # Get the time dimensions; ntimes=# of samples; nyrs=# of calendar years; nlead=# of foreecast leads
+    ntimes=len(time)
+    index=ds.time['time.year']
+    yStrt=index[0]
+    yLast=index[-1]
+    nyrs=int(yLast-yStrt)
+    nlead=int(len(index)/nyrs)
+    
+    # Compute climatology for each forecast lead 
+    climo=[]
+    for i in range(nlead):
+        #AMJ 12/22/22 fixed indexing bug
+        #da=ds[i:ntimes-1:nlead,:,:].mean(dim='time')
+        da=ds[i:ntimes:nlead,:,:].mean(dim='time')
+        climo.append(da.to_dataset(name='clim'))
+    ds_clim=xr.combine_nested(climo,concat_dim='time')
+    
+    # Change time coordinate from calendar dates to integers [0:ntimes]
+    days=[]
+    for i in range(nyrs):
+        days.append(np.arange(nlead))
+    days=np.reshape(days,nyrs*nlead)
+    
+    ds['time']=days
+    
+    # Compute anomalies 
+    anoms=ds.groupby(ds.time)-ds_clim
+    
+    # Rename anomaly 
+    anom=anoms['clim'].rename(anom_name)
+    
+    # Remap time coordinate to original calendar dates
+    anom['time']=time
+    
+    return anom
+  
+def regrid_scalar_spharm ( data_in , grid_input , grid_output ) : # This function is from the original code provided by Cheng
+    input_reorder  = np.transpose ( data_in , ( 1 , 2 , 0 ) )
+    regrid_data = spharm.regrid ( grid_input , grid_output , input_reorder )
+    regrid_reorder = np.transpose ( regrid_data , ( 2 , 0 , 1 ) )
+    return regrid_reorder
+
+def regrid_scalar_spharm_AJ(data, lat_in, lon_in, lat_out, lon_out):
+    '''
+    Regrid global scalar data using spherical harmonics.
+    
+        Parameters:
+            data:    xarray data array of gridded data to be regridded. 
+                     coordinate dimensions must be named 'latitude', 
+                     and 'longitude'. Dimensions can be 2D, 3D, or 4D.
+            lat_in:  latitudes corresponding to input data 
+            lon_in:  longitudes corresponding to input data
+            lat_out: latitudes corresponding to regridded data (xarray data array)
+            lon_out: longitudes corresponding to regridded data (xarray data array)
+            
+        Returns:
+            data_regrid: regridded data. note order of dimensions will be 
+                         re-arranged so that latitude & longitude are leading
+    '''
+    # dimension names
+    dims = data.dims
+    dims_not_lat_lon = list(set(['latitude','longitude']) ^ set(dims))
+    
+    ingrid = spharm.Spharmt(len(lon_in), len(lat_in), gridtype='regular')
+    outgrid = spharm.Spharmt(len(lon_out), len(lat_out), gridtype='regular')
+    
+    # From spharm documentation: input data dimensionality needs to be 
+    # (lat, lon, ...), and max. 3D
+    data = data.transpose('latitude','longitude',...)
+    
+    if data.ndim>2:
+        # Works for 3D or 4D data
+        data_regrid=[]
+        for ii in range(len(data[dims_not_lat_lon[0]])):
+            data_regrid.append(spharm.regrid(ingrid,outgrid,
+                                        data.isel(**{dims_not_lat_lon[0]: ii}).values))
+            
+        data_regrid = np.asarray(data_regrid) # list of arrays to one array
+        
+        # Construct data array from numpy array
+        data_regrid = xr.DataArray(data=data_regrid,dims=dims)
+            
+        # Assign the remaining coordinates
+        data_regrid['latitude'] = lat_out
+        data_regrid['longitude'] = lon_out
+        
+        for dim in dims_not_lat_lon:
+            data_regrid[dim]=data[dim]
+            
+    else:
+        data_regrid=spharm.regrid(ingrid,outgrid,data.values)
+        
+        # Construct data array from numpy array
+        data_regrid = xr.DataArray(data=data_regrid,
+                                   coords={'latitude':lat_out,
+                                           'longitude':lon_out})
+        
+    return data_regrid
+
+def regrid_vector_spharm ( u_input , v_input , grid_input , grid_output ) :   # This function is from the original code provided by Cheng
+    nlat = grid_input.nlat
+    nlon = grid_input.nlon
+    nlat_out = grid_output.nlat
+    nlon_out = grid_output.nlon
+    if nlat%2:                              # nlat is odd
+        n2 = (nlat + 1)/2
+    else:
+        n2 = nlat/2
+    nt = u_input.shape[2]
+    ntrunc = grid_output.nlat - 1
+    w = u_input
+    v = - v_input
+    lwork = (2*nt+1)*nlat*nlon
+    lwork_out = (2*nt+1)*nlat_out*nlon_out
+    br,bi,cr,ci,ierror = _spherepack.vhaes(v,w,grid_input.wvhaes,lwork)
+    bc = _spherepack.twodtooned(br,bi,ntrunc)
+    cc = _spherepack.twodtooned(cr,ci,ntrunc)
+    br_out, bi_out = _spherepack.onedtotwod(bc,nlat_out)
+    cr_out, ci_out = _spherepack.onedtotwod(cc,nlat_out)
+    v_out, w_out, ierror = _spherepack.vhses(nlon_out,br_out,bi_out,cr_out,ci_out,grid_output.wvhses,lwork_out)
+    return w_out,-v_out
+
+def interpolate_scalar(ds_in,nlon_out,nlat_out,grid_type,var_name):
+    
+    #grid_type: "regular", "gaussian"
+    
+    # Coordinates of field to be regrided
+    nlon_in = len(ds_in.longitude)
+    nlat_in = len(ds_in.latitude)
+    
+    # Define the input and output grids
+    ingrid = spharm.Spharmt ( nlon_in  , nlat_in  , gridtype=grid_type )
+    outgrid = spharm.Spharmt ( nlon_out , nlat_out , gridtype=grid_type )
+    
+    delat = 180. / ( nlat_out - 1 )
+    lat_out = 90 - np.arange ( nlat_out ) * delat
+    delon = 360. / nlon_out
+    lon_out = np.arange ( nlon_out ) * delon
+    
+    data_in_pre_regrid=np.empty([1, nlat_in, nlon_in])
+    data_regrid=np.empty([len (ds_in.time),nlat_out,nlon_out])
+    for i in range(len (ds_in.time)):
+        data_in_pre_regrid[0,:,:]=ds_in[i,:,:]
+        data_regrid[i,:,:]=regrid_scalar_spharm(data_in_pre_regrid,ingrid,outgrid)
+    output_regrid=xr.DataArray(data_regrid, name=var_name,
+                               dims=[ 'time','latitude','longitude'],
+                               coords=dict(time=ds_in.time,latitude=lat_out,longitude=lon_out),
+                               attrs=ds_in.attrs)
+    output_regrid['time']=ds_in.time
+    return output_regrid 
+
+# Interpolation function for vector fields to be added
+
+def reshape_forecast(fc,nfc=35):
+    ''' Reshape forecast data so the time dimension
+    is 2D with dimensions initialization day & forecast day. 
+    ---
+    Inputs
+    ---
+    fc: xarray data array of forecast data with merged time dimension named "time"
+    nfc: number of forecast days, default=35. Must be integer
+    
+    ---
+    Returns
+    ---
+    fc_reshape: reshaped xarray of forecast data with 2D time
+    '''
+    
+    # Check to make sure nfc is an integer
+    if not isinstance(nfc, int):
+        print('warning: nfc is not an integer. forcing integer type')
+        nfc = int(nfc)
+    
+    # Initialization days
+    init_day = fc.time[::nfc]
+    
+    # Forecast lead time
+    fc_day = np.arange(1,nfc+1)
+    
+    # Reshape
+    fc_reshape=[]
+    for iinit in range(len(init_day)):
+        # Subset and rename the forecast dimension
+        subset = fc.isel(time=np.arange(iinit*nfc,iinit*nfc+nfc))
+        subset = subset.rename(time='forecast_day')
+        subset['forecast_day']=fc_day
+        fc_reshape.append(subset)
+        
+    # Convert to single xarray data array from list of multiple data arrays
+    fc_reshape=xr.concat(fc_reshape,dim='time')
+    fc_reshape['time']=init_day
+    fc_reshape.time.attrs['long_name']='initial time'
+    
+    return fc_reshape
+
+def is_ndjfm(month):
+    ''' Returns true if winter month
+    '''
+    return (month >= 11) | (month <= 3)
