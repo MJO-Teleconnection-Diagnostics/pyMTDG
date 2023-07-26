bfe15f7784d618010e586f871c4e756df52d763a
Cristiana Stan <cstan@gmu.edu>
2023-07-26 19:35:49 -0400

    Added figure saving as jpg

--- T2m_composites/t2m_utils.py
+++ T2m_composites/t2m_utils.py
@@ -22,17 +22,18 @@ def is_day21(day):
 def is_day26(day):
     return (day == 26)
 
-def plotComposites(ds,levels,cmap,lon_0,lat_0,sig_map):
+def plotComposites(ds,levels,cmap,lon_0,lat_0,sig_map,fig_name):
     fig=plot.figure(refwidth=4.5)
     ax=fig.subplot(proj='npstere',proj_kw={'lon_0': lon_0})
     ax.contourf(ds,cmap=cmap,colorbar='b', lw=1,ec='none',extend='both',levels=levels)
     ax.contourf(ds.longitude,ds.latitude,sig_map,levels=[0,1],
                 colors='None',hatches=['...',''])
     ax.format(coast='True',boundinglat=lat_0,grid=False)
+    fig.savefig(fig_name+'.jpg',dpi=300)
   
 def correlate(obs,model,lat_min,lat_max,lon_min,lon_max):
-    x=obs.sel(latitude=slice(lat_max,lat_min),longitude=slice(lon_min,lon_max))
-    y=model.sel(latitude=slice(lat_max,lat_min),longitude=slice(lon_min,lon_max))
+    x=obs.sel(latitude=slice(lat_min,lat_max),longitude=slice(lon_min,lon_max),method = 'nearest')
+    y=model.sel(latitude=slice(lat_min,lat_max),longitude=slice(lon_min,lon_max),method = 'nearest')
     x_stacked=x.stack(grid=('latitude','longitude'))
     y_stacked=y.stack(grid=('latitude','longitude'))
     corr=np.corrcoef(x_stacked,y_stacked)
