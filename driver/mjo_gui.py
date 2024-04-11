a1e38480adee4e88c224577ca527ccfd2c23a5c0
Cristiana Stan <cstan@gmu.edu>
2024-04-11 15:52:04 -0400

    Added captions for MJO figures

--- driver/mjo_gui.py
+++ driver/mjo_gui.py
@@ -2277,7 +2277,7 @@ class mjoResult(QMainWindow):
         self.helpTexts=['''
         MJO index forecast skill: MJO prediction skill for UFS5, 6, 7, 8 reforecasts initialized with active MJO events during boreal winter (NDJFM). The prediction skill is evaluated based on the anomaly correlation coefficient (ACC, solid lines) and root-mean squared error (RMSE, dashed lines) between the model and observed RMM indices. The gray solid horizontal line indicates ACC of 0.5 and RMSE of 1.5.
         ''','''
-        Longitude-time composite: Longitude-time composites of OLR (W/m2; shading) and U850 (contour; interval 0.3 m/s) anomalies averaged over 15°S-15°N for active MJO events. The vertical lines indicate 120ºE (approximately the center of the Maritime Continent), respectively. A 5-day moving average is applied.
+        Longitude-time composite: Longitude-time composites of OLR (W/m2; shading) and U850 (contour; interval 0.3 m/s) anomalies averaged over 15S-15N for active MJO events. The vertical lines indicate 120E (approximately the center of the Maritime Continent), respectively. A 5-day moving average is applied.
         ''']
         for i in range(len(self.all_files)):
             buttonn=QPushButton(f'MJO Fig.{i+1}', self)
